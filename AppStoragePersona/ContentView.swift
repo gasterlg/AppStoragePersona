a7bc820b1e04a3a94080f97d2c22f59666214c1d
Luca Grilli <luca@lucagrilli.com>
2022-01-18 15:38:58 +0800

    First Commit

--- AppStoragePersona/ContentView.swift
+++ AppStoragePersona/ContentView.swift
@@ -8,9 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State private var nome = ""
+    @AppStorage("persona") var persona = ""
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 20) {
+            TextField("Inserisci nome", text: $nome)
+                .multilineTextAlignment(.center)
+                .frame(maxWidth: 200)
+            Button("Salva nome") {
+                persona = nome
+            }
+            Text("Persona salvata: \(persona)")
+        }
+        .frame(width: 400, height: 100)
+        .padding()
     }
 }
 
