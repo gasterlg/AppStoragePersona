7b0d06b62a6584c884e3444dad17b18313a645a8
Luca Grilli <luca@lucagrilli.com>
2022-01-18 15:34:09 +0800

    Initial Commit

---
+++ AppStoragePersona/AppStoragePersonaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStoragePersonaApp.swift
+//  AppStoragePersona
+//
+//  Created by Luca on 1/18/22.
+//
+
+import SwiftUI
+
+@main
+struct AppStoragePersonaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
